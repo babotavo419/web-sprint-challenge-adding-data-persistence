098dc5789073eb25f045e51ae6f96abb66e1795d
babotavo419 <one.faith419@gmail.com>
2023-08-09 14:39:15 -0400

    ok

--- api/server.js
+++ api/server.js
@@ -2,10 +2,21 @@ const express = require('express');
 
 const server = express();
 
+// Import the routers
+const projectsRouter = require('./project/router');
+const resourcesRouter = require('./resource/router');
+const tasksRouter = require('./task/router');
+
 server.use(express.json());
+
+// Attach the routers to specific endpoints
+server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourcesRouter);
+server.use('/api/tasks', tasksRouter);
 
 server.get('/', (req, res) => {
   res.json({ message: 'Server is running' });
 });
 
 module.exports = server;
+
